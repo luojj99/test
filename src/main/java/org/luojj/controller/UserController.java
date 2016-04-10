23e148c70d8c7fa16903bc827a9f8ef97d60d9fc
luojj99 <luojj99@gmail.com>
2016-04-11 01:34:29 +0800

    test login

--- src/main/java/org/luojj/controller/UserController.java
+++ src/main/java/org/luojj/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.luojj.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.apache.log4j.Logger;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 
 
@@ -21,17 +25,37 @@ public class UserController {
     private IUserService userService;
 	
     @RequestMapping(value="/login/phoneNumber/{phoneNumber}/loginPassword/{loginPassword}",method=RequestMethod.GET)
-    public @ResponseBody User login(@PathVariable String phoneNumber,
+    public  ModelAndView login(@PathVariable String phoneNumber,
     		@PathVariable String loginPassword)  {
         User user=userService.checkLogin(phoneNumber,loginPassword);
+        System.out.println(22222);
         if(user!=null){
+        	ModelAndView modelView=new ModelAndView(); 
+            Map<String,Object> modelMap=new HashMap<String,Object>(); 
+            modelMap.put("loginPassword", loginPassword);
+            modelMap.put("phoneNumber", phoneNumber);
+            modelMap.put("user", user);
+        	modelView.addAllObjects(modelMap);  
         	logger.info("login success");
-        	
-            return user;          
+        	System.out.println(1111111111);
+            return modelView;          
         }
         logger.info("login fail");
         return null;
     }
+    
+    
+//    public ModelAndView queryFoodList(@RequestParam("typeId") int typeId){  
+//        
+//        List<Food> list=foodService.getFoodListByType(typeId);  
+//        ModelAndView modelView=new ModelAndView();  
+//        Map<String,Object> modelMap=new HashMap<String,Object>();  
+//        modelMap.put("FOOD_LIST", list);  
+//        modelMap.put("TYPE_ID", typeId);  
+//        modelView.addAllObjects(modelMap);  
+//        return modelView;  
+//    }  
+    
     
     @RequestMapping(value="/register/phoneNumber/{phoneNumber}/loginPassword/{loginPassword}",method=RequestMethod.GET)
     public @ResponseBody boolean register(@PathVariable String phoneNumber,
