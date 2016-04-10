93b70f34f3f72f21426fe8b26381d074242faa15
luojj99 <luojj99@gmail.com>
2016-04-11 01:04:36 +0800

    test unfind

--- src/main/java/org/luojj/controller/UserController.java
+++ src/main/java/org/luojj/controller/UserController.java
@@ -1,18 +1,17 @@
 package org.luojj.controller;
 
 import javax.annotation.Resource;
-import javax.mail.Flags.Flag;
 
 import org.apache.log4j.Logger;
 import org.luojj.model.User;
 import org.luojj.service.IUserService;
-import org.luojj.test.TestMybatis;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 @Controller
 public class UserController {
@@ -24,10 +23,10 @@ public class UserController {
     @RequestMapping(value="/login/phoneNumber/{phoneNumber}/loginPassword/{loginPassword}",method=RequestMethod.GET)
     public @ResponseBody User login(@PathVariable String phoneNumber,
     		@PathVariable String loginPassword)  {
-        User user=userService.checkLogin(phoneNumber, loginPassword);
+        User user=userService.checkLogin(phoneNumber,loginPassword);
         if(user!=null){
         	logger.info("login success");
-        	//fsdfs
+        	
             return user;          
         }
         logger.info("login fail");
