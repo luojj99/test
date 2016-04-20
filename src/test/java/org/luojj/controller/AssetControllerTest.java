96c9ccd8922515f880995694fec5bd393ab8f7c5
luojj99 <luojj99@gmail.com>
2016-04-20 15:46:37 +0800

    不在服务器测试

--- src/test/java/org/luojj/controller/AssetControllerTest.java
+++ src/test/java/org/luojj/controller/AssetControllerTest.java
@@ -1,63 +1,63 @@
-package org.luojj.controller;
-
-import static org.junit.Assert.*;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.math.BigDecimal;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.luojj.entity.Asset;
-import org.luojj.test.BaseTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
-
-import com.alibaba.fastjson.JSON;
-
-public class AssetControllerTest extends BaseTest{
-
-//	@Before
-//	public void setUp() throws Exception {
-//	}
-
+//package org.luojj.controller;
+//
+//import static org.junit.Assert.*;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import java.math.BigDecimal;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.luojj.entity.Asset;
+//import org.luojj.test.BaseTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.annotation.Rollback;
+//
+//import com.alibaba.fastjson.JSON;
+//
+//public class AssetControllerTest extends BaseTest{
+//
+////	@Before
+////	public void setUp() throws Exception {
+////	}
+//
+////	@Test
+////	public void testFixUpdateMethod() {
+////		fail("Not yet implemented");
+////	}
+//
+////	@Test
+////	public void testInsertAsset() {
+////		fail("Not yet implemented");
+////	}
+//
 //	@Test
-//	public void testFixUpdateMethod() {
-//		fail("Not yet implemented");
-//	}
-
-//	@Test
-//	public void testInsertAsset() {
-//		fail("Not yet implemented");
-//	}
-
-	@Test
-	
-	public void testUpdateAsset () throws Exception {
-		String phonNumber ="13800138000";
-		Asset asset = new Asset();
-		asset.setPhoneNumber(phonNumber);
-		BigDecimal balance = new BigDecimal("2000000000");
-		asset.setBalance(balance);
-		String jsonString=JSON.toJSONString(asset);
-//		  mockMvc.perform((post("/asset/update")
-//				 .param(jsonString)))
+//	
+//	public void testUpdateAsset () throws Exception {
+//		String phonNumber ="13800138000";
+//		Asset asset = new Asset();
+//		asset.setPhoneNumber(phonNumber);
+//		BigDecimal balance = new BigDecimal("2000000000");
+//		asset.setBalance(balance);
+//		String jsonString=JSON.toJSONString(asset);
+////		  mockMvc.perform((post("/asset/update")
+////				 .param(jsonString)))
+////				 .andExpect(status()
+////				 .isOk())
+////				 .andDo(print());  
+//		    super.mockMvc.perform(post("/asset/update")
+//				 .contentType(MediaType.APPLICATION_JSON)
+//				 .content(jsonString))
 //				 .andExpect(status()
-//				 .isOk())
-//				 .andDo(print());  
-		    super.mockMvc.perform(post("/asset/update")
-				 .contentType(MediaType.APPLICATION_JSON)
-				 .content(jsonString))
-				 .andExpect(status()
-		  		 .isOk())
-			     .andDo(print());  
-	}
-//fsdfs
-//	@Test
-//	public void testGetAsset() {
-//		fail("Not yet implemented");
+//		  		 .isOk())
+//			     .andDo(print());  
 //	}
-
-}
+////fsdfs
+////	@Test
+////	public void testGetAsset() {
+////		fail("Not yet implemented");
+////	}
+//
+//}
